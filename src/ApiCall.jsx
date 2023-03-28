e5eeea439818dce1a579e737a2dad8294e3ea70b
Facu002 <achafacundoagustin@gmail.com>
2023-03-28 20:12:50 -0300

    setting up the project

---
+++ src/ApiCall.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ApiCall() {
+    return (
+        <div class="bg-blue-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-auto">
+            <h1 class="text-pink-500">TESTING</h1>
+        </div>
+    )
+}
+
+export default ApiCall
